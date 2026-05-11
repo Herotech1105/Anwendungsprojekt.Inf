a73ae2c3d72aa33abfaa11fc1c00f3920e64eda2
barnabas-steiner <barnabas.stiener@live.de>
2026-05-11 10:40:09 +0200

    sql users syntax correction

--- mariadb/03_user.sql
+++ mariadb/03_user.sql
@@ -1,7 +1,7 @@
 CREATE USER 'websrv_write'@'websrv.lab.local'
-IDENTIFIED BY 'pass-0123-iot'
+IDENTIFIED BY 'pass-0123-iot';
 
 GRANT SELECT, INSERT ON myapp.sensordata
 TO 'websrv_write'@'websrv.lab.local';
 
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
