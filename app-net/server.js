3dca4064165183b1377cad917d2c9b1387695a7f
Lennart Esch <lennart.esch@innosystec.de>
2026-05-12 09:28:31 +0200

    format timestamp in webserver to match expected mariadb type

--- app-net/server.js
+++ app-net/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config(); // use .env file
 const express = require('express');
 const app = express();
-const { getPool } = require('./config/database');
+const {getPool} = require('./config/database');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 
 
 // middleware - authenticate API-key
@@ -12,10 +12,10 @@ const authenticateApiKey = (req, res, next) => {
     const apiKey = req.header('x-api-key');
 
     if (!apiKey || apiKey !== process.env.API_KEY) {
-        return res.status(401).json({ error: "Non authorised API-key" });
+        return res.status(401).json({error: "Non authorised API-key"});
     }
-    
-    next(); 
+
+    next();
 };
 
 app.use(express.static('public'));
@@ -24,7 +24,7 @@ app.set('trust proxy', true);
 
 // get status of server
 app.get('/api/status', (req, res) => {
-    res.json({ status: 'online', timestamp: new Date() });
+    res.json({status: 'online', timestamp: new Date()});
 });
 
 app.listen(PORT, () => {
@@ -36,7 +36,7 @@ app.listen(PORT, () => {
 // @Post: save sensordata into database
 app.post('/api/internal/sensordata', authenticateApiKey, async (req, res) => {
     // validate data
-    const { temperature, humidity, timestamp} = validateSensorPayload(req.body);
+    const {temperature, humidity, timestamp} = validateSensorPayload(req.body);
 
     let conn;
     try {
@@ -44,16 +44,16 @@ app.post('/api/internal/sensordata', authenticateApiKey, async (req, res) => {
         const pool = await getPool();
         console.log(pool);
         conn = await pool.getConnection();
-        
+
         // Prepared Statement
         const sql = "INSERT INTO sensor_data (temperature, humidity, timestamp) VALUES (?, ?, ?)";
         const result = await conn.query(sql, [temperature, humidity, timestamp]);
 
-        res.status(201).json({ 
+        res.status(201).json({
             status: "ok"
         });
     } catch (err) {
-        res.status(400).json({ error: err.message || "invalid payload"});
+        res.status(400).json({error: err.message || "invalid payload"});
     } finally {
         if (conn) conn.release();
     }
@@ -62,32 +62,37 @@ app.post('/api/internal/sensordata', authenticateApiKey, async (req, res) => {
 
 // @function: validating the payload for the sensor_data table
 validateSensorPayload = (data) => {
-    const { temperature, humidity, timestamp } = data;
+    const {temperature, humidity, timestamp} = data;
 
     console.log(temperature, humidity, timestamp)
     // check if temperature and humidity are numbers
-    if(isNaN(temperature) || isNaN(humidity)){
+    if (isNaN(temperature) || isNaN(humidity)) {
         return null;
     }
 
     // check if timestamp is valid
     const date = new Date(timestamp);
-    if(!(date instanceof Date && !isNaN(date.getTime()))){
+    if (!(date instanceof Date && !isNaN(date.getTime()))) {
         return null;
     }
 
     // check if temperature and humidity are in valid ranges
-    if(temperature < 0 || temperature > 60 || humidity < 10 || humidity > 70){
+    if (temperature < 0 || temperature > 60 || humidity < 10 || humidity > 70) {
         return null;
     }
 
     // check if timestamp is still up-to-date
     const currentDate = new Date();
     const minuteDifference = Math.floor((currentDate - date) / 60000);
-    if(minuteDifference < -5 || minuteDifference > 60){
+    if (minuteDifference < -5 || minuteDifference > 60) {
         return null;
     }
-    
+
+
+    const formated_timestamp = date.toISOString().slice(0, 19).replace('T', ' ')
     // return valid data
-    return {temperature, humidity, timestamp};
+    // Source - https://stackoverflow.com/a/11150727
+    return {
+        temperature, humidity, formated_timestamp
+    };
 }
