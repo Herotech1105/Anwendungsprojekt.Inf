98302238677a0b510404cdf0a818f7d2994162e5
Lennart Esch <lennart.esch@innosystec.de>
2026-05-22 11:41:45 +0200

    webapp print pool for debug

--- app-net/config/database.js
+++ app-net/config/database.js
@@ -18,6 +18,7 @@ async function getWritePool() {
             connectionLimit: 5
         });
     }
+    console.log(pool);
     return pool;
 }
 
@@ -32,6 +33,7 @@ async function getReadPool() {
             connectionLimit: 5
         });
     }
+    console.log(pool);
     return pool;
 }
 
