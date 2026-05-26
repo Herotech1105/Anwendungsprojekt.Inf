3a7ea03fca0ec544606cd510d45c84641d3ff347
Lennart Esch <lennart.esch@innosystec.de>
2026-05-26 12:28:35 +0200

    Temporary disables certificate verification in controller

--- sensor-net/keycloak_auth.py
+++ sensor-net/keycloak_auth.py
@@ -34,7 +34,7 @@ def request_token() -> str:
         KC_TOKEN_URL,
         data=data,
         timeout=HTTP_TIMEOUT,
-        verify=CA_CERT_PATH,
+        verify=False,
     )
     resp.raise_for_status()
     body = resp.json()
