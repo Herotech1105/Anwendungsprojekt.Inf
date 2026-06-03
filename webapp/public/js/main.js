9f105a96f7d2b51e3340b9ed9de794957b5e9a11
NussWal <walter-neer@web.de>
2026-06-03 14:55:51 +0200

    javascript fix, index.html refactor

--- webapp/public/js/main.js
+++ webapp/public/js/main.js
@@ -1,9 +1,7 @@
 import { initAuth } from "./auth.js";
 import { initThemeToggle } from "./theme.js";
 import { initEvents } from "./events.js";
-import { initChartEvents } from "./chart.js";
 
 initAuth();
 initThemeToggle();
 initEvents();
-initChartEvents();
