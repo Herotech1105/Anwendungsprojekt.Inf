a9e5ebb1e3424a421a9b3fe970d712dd0ac8d34d
NussWal <walter-neer@web.de>
2026-06-10 11:13:17 +0200

    export name for csv

--- webapp/public/js/frontend/api.js
+++ webapp/public/js/frontend/api.js
@@ -31,7 +31,7 @@ export async function exportDatabase() {
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "myapp_export.sql";
+    a.download = `myapp_export${new Date().toString()}.csv`;
     a.click();
     URL.revokeObjectURL(url);
 }
